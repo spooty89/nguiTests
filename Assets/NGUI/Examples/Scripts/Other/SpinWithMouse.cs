2ceb2e80b987b8e426c9ef98435ac0b2df036f47
Matt <mattbb987@gmail.com>
2014-07-31 20:05:26 -0400

    Initial commit

---
+++ Assets/NGUI/Examples/Scripts/Other/SpinWithMouse.cs
@@ -0,0 +1,98 @@
+using UnityEngine;
+
+[AddComponentMenu("NGUI/Examples/Spin With Mouse")]
+public class SpinWithMouse : MonoBehaviour
+{
+	public Transform target;
+	public float speed = 1f;
+
+	Transform mTrans;
+
+	public bool smoothDragStart = true;
+	public float momentumAmount = 35f;
+	bool dragging;
+	protected Vector2 mMomentum = Vector2.zero;
+	protected Vector2 mLastPos = Vector2.zero;
+	protected Vector2 mDragStartOffset = Vector2.zero;
+	protected Plane mPlane;
+
+	void Start ()
+	{
+		mTrans = transform;
+	}
+
+	void OnPress(bool pressed) {
+		mPlane = new Plane(UICamera.currentCamera.transform.rotation * Vector3.back, mLastPos);
+	}
+
+	void OnDragStart() {
+		dragging = true;
+		mDragStartOffset = smoothDragStart ? UICamera.currentTouch.totalDelta : Vector2.zero;
+	}
+
+	void OnDrag (Vector2 delta)
+	{
+		UICamera.currentTouch.clickNotification = UICamera.ClickNotification.None;
+
+		if (target != null)
+		{
+			target.localRotation = Quaternion.Euler(0.5f * delta.y * speed, -0.5f * delta.x * speed, 0f) * target.localRotation;
+		}
+		else
+		{
+			mTrans.localRotation = Quaternion.Euler(0.5f * delta.y * speed, -0.5f * delta.x * speed, 0f) * mTrans.localRotation;
+		}
+		
+		
+		Ray ray = smoothDragStart ?
+			UICamera.currentCamera.ScreenPointToRay(UICamera.currentTouch.pos - mDragStartOffset) :
+				UICamera.currentCamera.ScreenPointToRay(UICamera.currentTouch.pos);
+		
+		float dist = 0f;
+		
+		if (mPlane.Raycast(ray, out dist))
+		{
+			Vector2 currentPos = new Vector2(ray.GetPoint(dist).x, ray.GetPoint(dist).y);
+			Vector2 offset = currentPos - mLastPos;
+			mLastPos = currentPos;
+			mMomentum = Vector2.Lerp(mMomentum, mMomentum + offset * (0.01f * momentumAmount), 0.67f);
+		}
+	}
+	
+	void OnDragEnd ()
+	{
+		dragging = false;
+	}
+
+	void LateUpdate() {
+		float delta = RealTime.deltaTime;
+		if (!dragging)
+		{
+			if (mMomentum.magnitude > 0.0001f)
+			{
+				Vector3 meh = UICamera.currentCamera.transform.TransformDirection(new Vector3(mMomentum.magnitude * 0.05f, mMomentum.magnitude * 0.05f, 0f));
+				mMomentum -= new Vector2(meh.x, meh.y);
+				
+				// Move the scroll view
+				Vector3 offset = NGUIMath.SpringDampen(ref mMomentum, 9f, delta);
+				if (target != null)
+				{
+					target.localRotation = Quaternion.Euler(0.5f * offset.y * speed, -0.5f * offset.x * speed, 0f) * target.localRotation;
+				}
+				else
+				{
+					mTrans.localRotation = Quaternion.Euler(0.5f * offset.y * speed, -0.5f * offset.x * speed, 0f) * mTrans.localRotation;
+				}
+			}
+			else
+			{
+				mMomentum = Vector3.zero;
+			}
+		}
+		else
+		{
+			// Dampen the momentum
+			NGUIMath.SpringDampen(ref mMomentum, 9f, delta);
+		}
+	}
+}
---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour {
+	public void MakeItRed() {
+		GetComponent<UIWidget>().color = Color.red;
+	}
+	public void MakeItGreen() {
+		GetComponent<UIWidget>().color = Color.green;
+	}
+	public void MakeItBlue() {
+		GetComponent<UIWidget>().color = Color.blue;
+	}
+}
